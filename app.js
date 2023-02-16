9108fe1f4e169343e4c74302e42311eef52d999f
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-16 22:51:18 +0430

    postman automatically takes care of headers when we pass json to before we did that manually

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express()
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from the server side...')
+    res.status(200).json({ message: 'Hello from the server side...', app: 'Natours' })
+})
+
+app.post('/', (req, res) => {
+    res.send('you can post to this endpoint...')
 })
 
 app.listen(port, () => {

3567a3fc3d1b480fbdc62db9cf05b57b9d280844
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-18 20:58:46 +0430

    previously we still got the 200 status code even for the tours that actually not existed in json file but by implementing a simple solution that part is done as well

--- app.js
+++ app.js
@@ -34,7 +34,12 @@ app.get('/api/v1/tours/:id', (req, res) => {
 
   const id = req.params.id * 1;
   const tour = tours.find(el => el.id === id);
-
+  if (!tour) {
+    return res.status(404).json({
+      status: "Fail",
+      message: "Invalid Id"
+    })
+  }
   res.status(200).json({
     status: 'success',
     data: {

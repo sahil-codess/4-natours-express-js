0e23f5c3ff763850e5b27db3363fd61a5b333c9d
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-18 10:13:34 +0430

    Handling post request in the API needs middleware and we can use express.json() to take the post

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs');
 
-const app = express()
+const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //     res.status(200).json({ message: 'Hello from the server side...', app: 'Natours' })
@@ -12,18 +14,25 @@ const port = 3000;
 //     res.send('you can post to this endpoint...')
 // })
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status: "success",
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
-})
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done!');
+});
 
 app.listen(port, () => {
-    console.log(`app running on port ${port}...`);
-})
+  console.log(`app running on port ${port}...`);
+});

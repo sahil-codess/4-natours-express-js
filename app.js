8dad0caf0cfbb1b01dc5fe7cb2953caee05607e4
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-18 21:26:43 +0430

    handling patch request with express

--- app.js
+++ app.js
@@ -67,6 +67,22 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "Fail",
+      message: "Invalid Id"
+    })
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "<Updated the tour here...>"
+    }
+  })
+})
+
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
 });

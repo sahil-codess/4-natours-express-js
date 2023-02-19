861b5135b6cb6cc7845e67231446892967cd45aa
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-19 10:32:13 +0430

    creating my first middleware and it absolutely neccessary to call the next() function in each middleware that we create

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+app.use((req, res, next) => {
+  console.log('Hello from the middleware...😘');
+  next();
+});
+
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -17,26 +22,26 @@ const getAllTours = (req, res) => {
       tours,
     },
   });
-}
+};
 
 const getTour = (req, res) => {
   console.log(req.params);
 
   const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id);
+  const tour = tours.find((el) => el.id === id);
   if (!tour) {
     return res.status(404).json({
-      status: "Fail",
-      message: "Invalid Id"
-    })
+      status: 'Fail',
+      message: 'Invalid Id',
+    });
   }
   res.status(200).json({
     status: 'success',
     data: {
-      tour
+      tour,
     },
   });
-}
+};
 
 const createTour = (req, res) => {
   //   console.log(req.body);
@@ -55,37 +60,37 @@ const createTour = (req, res) => {
       });
     }
   );
-}
+};
 
 const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
-      status: "Fail",
-      message: "Invalid Id"
-    })
+      status: 'Fail',
+      message: 'Invalid Id',
+    });
   }
 
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: {
-      tour: "<Updated the tour here...>"
-    }
-  })
-}
+      tour: '<Updated the tour here...>',
+    },
+  });
+};
 
 const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
-      status: "Fail",
-      message: "Invalid Id"
-    })
+      status: 'Fail',
+      message: 'Invalid Id',
+    });
   }
 
   res.status(204).json({
-    status: "success",
-    data: null
-  })
-}
+    status: 'success',
+    data: null,
+  });
+};
 
 // app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);

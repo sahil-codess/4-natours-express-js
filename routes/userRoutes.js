92b105f36e087a7ce766c148e26f178528aef86d
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-20 10:57:39 +0430

    controller folder is where we put the handler function request based on MVC archeticture

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,44 +1,17 @@
 const express = require('express');
 
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This rout is not yet defined',
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This rout is not yet defined',
-  });
-};
-
-const creatUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This rout is not yet defined',
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This rout is not yet defined',
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This rout is not yet defined',
-  });
-};
-
 const router = express.Router();
+const userController = require('./../controllers/userController');
 
 // Mounting multiple routes
-router.route('/').get(getAllUsers).post(creatUser);
-router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.creatUser);
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;

ef271da413c4aa97e4ac6472a080a05dc21a813d
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-21 11:45:00 +0430

    just a console log

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
+dotenv.config({ path: './config.env' });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

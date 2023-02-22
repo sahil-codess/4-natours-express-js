b6737d8ee8740c904e1353e7df4701485cadb854
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-22 12:22:48 +0430

    setted up mongo db for the app and made the connection for it

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
 dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.set('strictQuery', false);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection established !'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

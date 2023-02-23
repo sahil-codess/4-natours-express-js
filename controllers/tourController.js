ea6b6b93c7e73052d58e7a7ca38b002a95f2b519
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-23 11:02:13 +0430

    creating documents using mongoose, basically creating tours with async await that returns a promise and we store it in a variable and send it to client and we have try catch block for catching error

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -7,16 +7,6 @@ const Tour = require('./../models/tourModel');
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'Fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestedTime);
   res.status(200).json({
@@ -48,13 +38,21 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'Fail',
+      message: err.message,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {

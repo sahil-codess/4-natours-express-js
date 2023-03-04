60390fda61385aa4b1c33b6d338777167862bf76
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-03-04 10:26:56 +0430

    with group stage we can extract the number of tours based on its month using mongo db month method

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -156,6 +156,20 @@ exports.getMonthlyPlan = async (req, res) => {
       {
         $unwind: '$startDates',
       },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStart: { $sum: 1 },
+        },
+      },
     ]);
 
     res.status(200).json({

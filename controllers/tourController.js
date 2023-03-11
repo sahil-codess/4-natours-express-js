482ccda2872aa9619a0fb252d75a9e97867ccf3a
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-03-11 23:20:51 +0430

    adding 404 page for the queries that return null in postman like getting a tour by ID that does not exist

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError');
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
@@ -38,6 +39,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
 
+  if (!tour) {
+    return next(new AppError('No tour was found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -63,6 +68,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true,
   });
 
+  if (!tour) {
+    return next(new AppError('No tour was found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -72,7 +81,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour was found with that ID', 404));
+  }
 
   res.status(204).json({
     status: 'success',

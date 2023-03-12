3122646feb2368d798fbaaaac93957f93c27d443
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-03-12 19:42:41 +0430

    validating the invalid data input

--- controllers/errorControllers.js
+++ controllers/errorControllers.js
@@ -11,6 +11,13 @@ const handleDuplicateErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = err => {
+  const error = object.values(error.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -51,6 +58,7 @@ module.exports = (err, req, res, next) => {
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateErrorDB(error);
-    sendErrorProd(err, res);
+    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+    sendErrorProd(error, res);
   }
 };

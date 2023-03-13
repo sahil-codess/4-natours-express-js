777b3e7e47235a586da3de0ebb9d836aee8d4884
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-03-13 12:10:56 +0430

    handling invalid id query by user

--- controllers/errorControllers.js
+++ controllers/errorControllers.js
@@ -1,4 +1,11 @@
-const sendErrorDev = (err, req) => {
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
@@ -21,7 +28,7 @@ const sendErrorProd = (err, res) => {
     console.error('Error occured', err);
     // 2) send generic message
     res.status(500).json({
-      status: 'Error',
+      status: 'error',
       message: 'Something went wrong!...',
     });
   }
@@ -31,9 +38,12 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV !== 'development') {
+  if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
